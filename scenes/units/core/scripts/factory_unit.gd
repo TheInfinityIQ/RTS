087f18fd593115fcd42cbfebe478a172f999c748
Dawson K. <46154365+TheInfinityIQ@users.noreply.github.com>
2025-06-01 20:02:36 -0500

    Image now loads when Factory builds unit

--- scenes/units/core/scripts/factory_unit.gd
+++ scenes/units/core/scripts/factory_unit.gd
@@ -1,11 +1,15 @@
 extends Object
 class_name FactoryUnit
 
+const UnitScene = preload("res://scenes/units/core/Unit.tscn")
+
 static func build(unit_type: int, start_positon: Vector2, unit_id: int, unit_team: String) -> Unit:
 	var blueprint: BlueprintUnit = BlueprintMapper.map[unit_type]
-	var unit: Unit = Unit.new()
+	var unit: Unit = UnitScene.instantiate()
 	
 	blueprint.config(unit, unit_id, unit_team)
 	unit.position = start_positon
+	var sprite = unit.get_node("Sprite2D")
+	sprite.texture = load("res://assets/units/avatars/f_10.png")
 	
 	return unit
